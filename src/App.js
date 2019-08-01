88899511bb9f14ee533a63023c72dfdd40faac5c
Jukka Edvardsson <jukka.edvardsson@wunderdog.fi>
2019-08-01 09:38:12 +0300

    25. Fetching Content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,20 +7,14 @@ class App extends Component {
     super();
     this.state = {
       monsters: [
-        {
-          name: 'Frankestein',
-          id: 'asdf'
-        },
-        {
-          name: 'Dracula',
-          id: 'asdf1'
-        },
-        {
-          name: 'Zombie',
-          id: 'asdf2'
-        }
       ]
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({monsters: users}))
   }
 
   render() {
