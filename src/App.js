e32ebd4877c7e60f43c7e1b624e97b3619606a94
Jukka Edvardsson <jukka.edvardsson@wunderdog.fi>
2019-08-01 09:20:44 +0300

    22 Dynamic content lecture

--- src/App.js
+++ src/App.js
@@ -2,48 +2,34 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React now
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 class App extends Component {
 
   constructor(){
     super();
     this.state = {
-      string: 'Hello Jukka Edvardsson'
+      monsters: [
+        {
+          name: 'Frankestein',
+          id: 'asdf'
+        },
+        {
+          name: 'Dracula',
+          id: 'asdf1'
+        },
+        {
+          name: 'Zombie',
+          id: 'asdf2'
+        }
+      ]
     }
   }
 
   render() {
     return (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              {this.state.string}
-            </p>
-            <button onClick={()=> this.setState({string: 'Hello Andre'})}>
-              change text
-            </button>
-          </header>
+          {
+            this.state.monsters.map(monster => <h1 >{monster.name}</h1>)
+          }
         </div>
     )
   }
