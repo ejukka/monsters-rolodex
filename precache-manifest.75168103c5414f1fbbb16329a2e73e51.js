dbdf323b5be438c1a20b8133edd4e4c6e1cf6c18
Jukka Edvardsson <jukka.edvardsson@gmail.com>
2019-08-02 15:27:28 +0300

    Updates

---
+++ precache-manifest.75168103c5414f1fbbb16329a2e73e51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad7e633ccbe0aedb40abb346d01904b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e7411cfb905fe20d3f80",
+    "url": "/monsters-rolodex/static/css/main.8a01cc2c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e7411cfb905fe20d3f80",
+    "url": "/monsters-rolodex/static/js/main.19fafb1a.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
