f7782e6ea39b9fc43b476fc2276c78230261a6e5
Jukka Edvardsson <jukka.edvardsson@wunderdog.fi>
2019-08-02 13:38:24 +0300

    38. Class methods and Arrow functions

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ class App extends Component {
         .then(users => this.setState({monsters: users}))
   }
 
+  handleChange = e => {
+      this.setState({searchField: e.target.value});
+  }
+
 
   render() {
     const {monsters, searchField} = this.state;
@@ -27,7 +31,7 @@ class App extends Component {
 
     return (
         <div className="App">
-            <SearchBox placeholder="search monsters" handleChange={e => this.setState({searchField: e.target.value})} />
+            <SearchBox placeholder="search monsters" handleChange={e => this.handleChange(e)} />
           <CardList monsters={filteredMonsters}>
           </CardList>
         </div>
